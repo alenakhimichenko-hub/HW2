084a77ecd02705c59ca6b8c196782a9329e476ec
Alena <alena.khimichenko@gmail.com>
2026-06-04 04:49:30 +0300

    Дополнительно добавлены тесты на shoppinglist

--- test_recipes.py
+++ test_recipes.py
@@ -7,6 +7,7 @@ import pytest
 
 from recipe.ingredient import Ingredient
 from recipe.recipe import Recipe
+from recipe.shoppinglist import ShoppingList
 
 def test_creation():
     a = Ingredient("Яблоко", 30.0, "шт")
@@ -89,3 +90,59 @@ def test_uniq():
     r = Recipe("Шоколадка", [neww, new1, new1, new2, neww])
     assert len(r) == 3
 
+def test_shoppinglist_add():
+    arr = ShoppingList()
+    newr = Recipe("Nutella", [Ingredient("Сахар", 100.0, "г")])
+    arr.add_recipe(newr, 10)
+    assert arr.get_list()[0].quantity == 1000.0
+    f = False
+    try:
+        arr.add_recipe(newr, -10)
+    except ValueError:
+        f = True
+    assert f
+
+
+def test_shoppinglist_remove():
+    arr = ShoppingList()
+    arr.add_recipe(Recipe("Nutella", [Ingredient("Сахар", 100.0, "г")]), 1)
+    arr.remove_recipe("Nutella")
+    assert arr.get_list() == []
+    arr.add_recipe(Recipe("Nutella", [Ingredient("Сахар", 100.0, "г")]), 1)
+    arr.remove_recipe("Milka")
+    assert len(arr.get_list()) == 1
+
+
+def test_shoppinglist_get_list():
+    arr = ShoppingList()
+    newr1 = Recipe("Nutella", [Ingredient("Сахар", 100.0, "г")])
+    newr2 = Recipe("Milka", [Ingredient("Сахар", 200.0, "г")])
+    arr.add_recipe(newr1, 1)
+    arr.add_recipe(newr2, 2)
+    ress= arr.get_list()
+    assert len(ress) == 1
+    assert ress[0].name == "Сахар"
+    assert ress[0].quantity == 500.0
+
+
+def test_shoppinglist_get_listsort():
+    arr = ShoppingList()
+    newr = Recipe("Nutella", [Ingredient("Сахар", 100.0, "г"), Ingredient("Масло", 50.0, "г"), Ingredient("Какао", 30.0, "г"), Ingredient("Орехи", 40.0, "г")])
+    arr.add_recipe(newr, 1)
+    ress = arr.get_list()
+    ans = []
+    for i in range(len(ress)):
+        ans.append(ress[i].name)
+    assert ans== sorted(ans)
+
+def test_shoppinglist_add_new():
+    arr1 = ShoppingList()
+    arr2 = ShoppingList()
+    newr1 = Recipe("Nutella", [Ingredient("Сахар", 100.0, "г")])
+    newr2 = Recipe("Milka", [Ingredient("Масло", 50.0, "г")])
+    arr1.add_recipe(newr1, 1)
+    arr2.add_recipe(newr2, 1)
+    arr3 = arr1 + arr2
+    assert len(arr3._items) == 2
+    assert len(arr1._items) == 1
+    assert len(arr2._items) == 1
